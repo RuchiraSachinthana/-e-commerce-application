f9fca67e9580e57c8a811ecea41f663f41e42261
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-06-17 09:05:55 +0530

    updated

--- e-zone/src/redux/store.js
+++ e-zone/src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 import rootReducers from "./reducer";
 
-const store = createStore(rootReducers);
+const store = createStore(rootReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
